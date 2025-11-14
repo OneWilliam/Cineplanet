8e1388113c8f9ce95e7d58b7560c36af94b9503b
OneWilliam <wyvargac@unjbg.edu.pe>
2025-11-14 10:02:51 -0500

    fix: root

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <div>Hola Mundo!</div>
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>CINEPLANET - Sistema de Gestión de Cines</title>
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <h1>CINEPLANET</h1>
+            </header>
+        </div>
+    </body>
+</html>
